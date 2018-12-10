970009f533fc9ba4eb303f6339d5ea0623191f2d
xiyacao <xc2402@columbia.edu>
2018-12-10 17:46:22 -0500

    added plots

--- dataset_generator.py
+++ dataset_generator.py
@@ -15,6 +15,8 @@ def load_shoe_data(sub, side='r', data_path='/home/xiya/ROAR/data/ShoesData/save
     length = min(shoe.shape[0], label.shape[0])
     shoe = shoe[:length, :]
     label = label[:length]
+    # import IPython
+    # IPython.embed()
     return shoe, label
 
 def sample_shoe_data(sub, samples=1000, side='r', data_path='/home/xiya/ROAR/data/ShoesData/saveDB/'):
@@ -34,4 +36,29 @@ def scale_data(data):
 		channel_scale = max(data[:,i]) - min(data[:,i])
 		data[:, i] = (data[:, i] - channel_mean) / channel_scale
 	return data
+
+def scale_channel(data):
+	# data in the form: data_points * channels
+	channel_mean = np.mean(data.flatten())
+	channel_scale = max(data.flatten()) - min(data.flatten())
+	data = (data - channel_mean) / channel_scale
+	return data
+
+
+def sample_back_shoe_data(sub, samples=10000, sample_back=50, side='r', data_path='/home/xiya/ROAR/data/ShoesData/saveDB/'):
+	shoe, label = load_shoe_data(sub, side, data_path)
+	length = len(label)
+	channels = shoe.shape[1]
+	indexes = np.random.choice(length-sample_back, samples) + sample_back
+	sampled_shoe = np.zeros((samples, sample_back, channels))
+	for i in range(samples):
+		sampled_shoe[i,:,:] = shoe[indexes[i]-sample_back:indexes[i], :]
+	for i in range(channels):
+		sampled_shoe[:,:,i] = scale_channel(sampled_shoe[:,:,i])
+	sampled_shoe = sampled_shoe.reshape((samples, sample_back*channels))
+	sampled_label = label[indexes]
+	#import IPython
+	#IPython.embed()
+	return sampled_shoe, sampled_label
+
 
