cd58939f50612e01db5397db89113d53cb2c48f5
xiyacao <xc2402@columbia.edu>
2018-12-09 19:53:24 -0500

    added plot tsne

---
+++ plot_tsne.py
@@ -0,0 +1,83 @@
+from time import time
+
+import matplotlib.pyplot as plt 
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.ticker import NullFormatter
+
+from sklearn import manifold, datasets
+import dataset_generator
+
+shoe, color = dataset_generator.sample_shoe_data(1)
+shoe_scale = dataset_generator.scale_data(shoe)
+n_points = len(color)
+# import IPython
+# IPython.embed()
+n_neighbors = 15
+n_components = 2
+
+fig = plt.figure(figsize=(15, 8))
+# plot raw data
+ax = fig.add_subplot(241, projection='3d')
+ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=color, cmap=plt.cm.Spectral)
+ax.view_init(4, -72)
+plt.title("pressure")
+
+ax = fig.add_subplot(242, projection='3d')
+ax.scatter(X[:, 3], X[:, 4], X[:, 5], c=color, cmap=plt.cm.Spectral)
+ax.view_init(4, -72)
+plt.title("accelerate")
+
+ax = fig.add_subplot(243, projection='3d')
+ax.scatter(X[:, 6], X[:, 7], X[:, 8], c=color, cmap=plt.cm.Spectral)
+ax.view_init(4, -72)
+plt.title("euler")
+
+t0 = time()
+tsne = manifold.TSNE(n_components=n_components, init='pca', random_state=0)
+Y = tsne.fit_transform(X)
+t1 = time()
+print("t-SNE: %.2g sec" % (t1 - t0))
+ax = fig.add_subplot(248)
+plt.scatter(Y[:, 0], Y[:, 1], c=color, cmap=plt.cm.Spectral)
+plt.title("t-SNE")
+ax.xaxis.set_major_formatter(NullFormatter())
+ax.yaxis.set_major_formatter(NullFormatter())
+plt.axis('tight')
+
+t0 = time()
+tsne = manifold.TSNE(n_components=n_components, init='pca', random_state=0)
+Y = tsne.fit_transform(X)
+t1 = time()
+print("t-SNE: %.2g sec" % (t1 - t0))
+ax = fig.add_subplot(248)
+plt.scatter(Y[:, 0], Y[:, 1], c=color, cmap=plt.cm.Spectral)
+plt.title("t-SNE")
+ax.xaxis.set_major_formatter(NullFormatter())
+ax.yaxis.set_major_formatter(NullFormatter())
+plt.axis('tight')
+
+t0 = time()
+tsne = manifold.TSNE(n_components=n_components, init='pca', random_state=0)
+Y = tsne.fit_transform(X)
+t1 = time()
+print("t-SNE: %.2g sec" % (t1 - t0))
+ax = fig.add_subplot(248)
+plt.scatter(Y[:, 0], Y[:, 1], c=color, cmap=plt.cm.Spectral)
+plt.title("t-SNE")
+ax.xaxis.set_major_formatter(NullFormatter())
+ax.yaxis.set_major_formatter(NullFormatter())
+plt.axis('tight')
+
+t0 = time()
+tsne = manifold.TSNE(n_components=n_components, init='pca', random_state=0)
+Y = tsne.fit_transform(X)
+t1 = time()
+print("t-SNE: %.2g sec" % (t1 - t0))
+ax = fig.add_subplot(248)
+plt.scatter(Y[:, 0], Y[:, 1], c=color, cmap=plt.cm.Spectral)
+plt.title("t-SNE")
+ax.xaxis.set_major_formatter(NullFormatter())
+ax.yaxis.set_major_formatter(NullFormatter())
+plt.axis('tight')
+
+plt.show()
